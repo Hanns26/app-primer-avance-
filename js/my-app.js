0fa2a4ffe07808e7cb7d088218cc72117ff640ea
Hanns26 <45185958+Hanns26@users.noreply.github.com>
2019-03-08 00:29:27 -0600

    1
    1

---
+++ js/my-app.js
@@ -0,0 +1,59 @@
+﻿// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'auto', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+			{
+		path: '/login/',
+    	url: 'login.html',
+    	name: 'login',
+  		},
+			{
+		path: '/perfil/',
+    	url: 'perfil.html',
+    	name: 'perfil',
+  		},
+			
+	]
+});
+// Export selectors engine
+var $$ = Dom7;
+
+// Create notification with close button
+var notificationWelcome = myApp.notification.create({
+  icon: '<i class="material-icons">info</i>',
+  title: '¡Bienvenida!',
+  subtitle: 'Aplicaciones con framework 7',
+  text: 'Alejandra Lucia Ortega',
+  closeButton: true,
+	closeTimeout: 3000,
+});
+$$('.open-welcome').on('click', function () {
+  notificationWelcome.open();
+});
+$$('.window').on('load', function () {
+  notificationWelcome.open();
+});
+
+
+
+
+
+
+
+
+ 
+
